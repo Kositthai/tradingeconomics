ea31007674962820b76848d8c0e7088ffd6487d7
Kositthai <vkositthai@outlook.com>
2023-11-18 20:32:49 +0100

    declare port value in index.js backend

--- nodejs/backend/index.js
+++ nodejs/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 
+const port = process.env.PORT || 8080
+
 const newsRouter = require('./routes/news')
 const unsplashRouter = require('./routes/unplash')
 
@@ -11,6 +13,6 @@ app.use(express.json())
 app.use('/', newsRouter)
 app.use('/', unsplashRouter)
 
-app.listen(5000, () => {
-  console.log('app listening on port 5000')
+app.listen(port, '0.0.0.0', () => {
+  console.log(`app listening on port ${port} `)
 })
